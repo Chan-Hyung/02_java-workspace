5e0767cf2190b9a762193bb2985a93d8b9456d1d
Chan-Hyung <homework201@naver.com>
2025-06-26 17:34:57 +0900

    Create VariablePractice1.java

---
+++ 01_Varialble/src/com/kh/practice1/func/VariablePractice1.java
@@ -0,0 +1,23 @@
+package com.kh.practice1.func;
+
+import java.util.Scanner;
+
+public class VariablePractice1 {
+	public void PrintVariable() {
+		Scanner sc = new Scanner(System.in);
+		
+		String name = sc.nextLine();
+		System.out.print("이름을 입력하세요 : ");
+		
+		String gender = sc.nextLine();
+		System.out.print("성별을 입력하세요(남/여) : ");
+		
+		int age = sc.nextInt();
+		System.out.print("나이를 입력하세요 : ");
+		
+		int height = sc.nextInt();
+		System.out.print("키를 입력하세요 : \n");
+		
+		System.out.print("키" + height + "인" + age + "살" + gender + "자" + name + "님 반갑습니다 ^^");
+	}
+}
