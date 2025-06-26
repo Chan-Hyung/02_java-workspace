6bb36a383e715f1769e90f29edc895c3105ec789
Chan-Hyung <homework201@naver.com>
2025-06-26 13:22:02 +0900

    Update Run.java

--- 01_Varialble/src/com/kh/run/Run.java
+++ 01_Varialble/src/com/kh/run/Run.java
@@ -1,13 +1,17 @@
 package com.kh.run;
 
 import com.kh.variable.A_Variable;
+import com.kh.variable.B_KeyboardInput;
 
 public class Run {
 	public static void main(String[] args) {
 		// 클래스명 별칭 = new 클래스명();
 		A_Variable a = new A_Variable();
 		//a.printVariable();
-		a.declareVariable();
+		//a.declareVariable();
+		B_KeyboardInput b = new B_KeyboardInput();
+		//b.InputTest1();
+		b.InputTest2();
 		
 	}
 }
