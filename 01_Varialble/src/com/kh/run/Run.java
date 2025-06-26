3f794397a74e85753f25ba18d652ab7a16599382
Chan-Hyung <homework201@naver.com>
2025-06-26 12:11:49 +0900

    Update Run.java

--- 01_Varialble/src/com/kh/run/Run.java
+++ 01_Varialble/src/com/kh/run/Run.java
@@ -2,7 +2,7 @@ package com.kh.run;
 
 import com.kh.variable.A_Variable;
 
-public class run {
+public class Run {
 	public static void main(String[] args) {
 		// 클래스명 별칭 = new 클래스명();
 		A_Variable a = new A_Variable();
