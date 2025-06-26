1e1899479e6dc1ad16a58d2ababc62a2d938eab5
Chan-Hyung <homework201@naver.com>
2025-06-26 11:50:45 +0900

    Create Run.java

---
+++ 01_Varialble/src/com/kh/run/Run.java
@@ -0,0 +1,13 @@
+package com.kh.run;
+
+import com.kh.variable.A_Variable;
+
+public class run {
+	public static void main(String[] args) {
+		// 클래스명 별칭 = new 클래스명();
+		A_Variable a = new A_Variable();
+		//a.printVariable();
+		a.declareVariable();
+		
+	}
+}
