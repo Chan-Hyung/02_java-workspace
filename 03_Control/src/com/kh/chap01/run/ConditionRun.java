c0eac544d24902525086cebe5c5237e246df2936
Chan-Hyung <homework201@naver.com>
2025-06-27 18:04:12 +0900

    Update ConditionRun.java

--- 03_Control/src/com/kh/chap01/run/ConditionRun.java
+++ 03_Control/src/com/kh/chap01/run/ConditionRun.java
@@ -8,7 +8,7 @@ public class ConditionRun {
 	public static void main(String[] args) {
 
 		A_If a = new A_If();
-		//a.method1();
+		a.method1();
 		//a.method2();
 		//a.method3();
 		//a.method4();
@@ -20,7 +20,7 @@ public class ConditionRun {
 		//b.method1();
 		//b.method2();
 		//b.method3();
-		b.method4();
+		//b.method4();
 				
 	}
 
