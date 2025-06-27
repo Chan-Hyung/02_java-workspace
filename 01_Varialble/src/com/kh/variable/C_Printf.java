9fdbdd2f6bbdcfe157140f53125fafb81329c15c
Chan-Hyung <homework201@naver.com>
2025-06-27 09:06:18 +0900

    Update C_Printf.java

--- 01_Varialble/src/com/kh/variable/C_Printf.java
+++ 01_Varialble/src/com/kh/variable/C_Printf.java
@@ -4,7 +4,7 @@ public class C_Printf {
 	// System.out.print(출력하고자 하는 값); 	// 출력만 하고 줄바꿈 발생x
 	// System.out.println(출력하고자 하는 값);	// 출력 후 줄바꿈 발생 o
 	
-	// Systeml.out.printf("출력을하고자하는 형식(포펫)", 출력하고자 하는 값);
+	// System.out.printf("출력을하고자하는 형식(포펫)", 출력하고자 하는 값);
 	// 출력하고자 하는 값을 제시한 형식에 맞춰서 출력만 진행 (줄바꿈x)
 	/*
 	 * 포펫 안에서 쓰일 수 있는 키워드
