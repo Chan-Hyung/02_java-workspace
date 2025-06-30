4e42811b41432163b08d669b73da3c2ead9bb4ce
Chan-Hyung <homework201@naver.com>
2025-07-01 00:14:28 +0900

    Create Run.java

---
+++ 03_Control/src/com/kh/practice/chap02/run/Run.java
@@ -0,0 +1,20 @@
+package com.kh.practice.chap02.run;
+
+import com.kh.practice.chap02.loop.LoopPractice;
+
+public class Run {
+
+	public static void main(String[] args) {
+		LoopPractice a = new LoopPractice();
+		//a.practice1();
+		//a.practice2();
+		//a.practice5();
+		//a.practice6();
+		//a.practice8();
+		//a.practice9();
+		//a.practice11();
+		//a.practice13();
+		a.practice14();
+	}
+
+}
