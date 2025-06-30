b041f68abcdd6267af83179c67e91c2a6ae2708e
Chan-Hyung <homework201@naver.com>
2025-06-30 16:42:46 +0900

    Create Student.java

---
+++ 05_Object/src/com/kh/chap01_abstraction/model/vo/Student.java
@@ -0,0 +1,29 @@
+package com.kh.chap01_abstraction.model.vo;
+
+/*
+ *  - 클래스의 구조
+ *  public class 클래스명 {
+ *  
+ *  // 필드부
+ *  
+ *  // 생성자부
+ *  
+ *  // 메소드부
+ *  
+ *  
+ *  }
+ */
+
+public class Student {// 학생을 추상화해서 만들 클래스
+	
+	// 필드부
+	// * 접근제한자 : 프로그래밍에 접근할 수 있는 범위를 제한하는 것
+	// 				public > protected > default > private
+	
+// 접근제한자 자료형 변수명	
+	public String name;
+	public int age;
+	public double height;
+	
+
+}
