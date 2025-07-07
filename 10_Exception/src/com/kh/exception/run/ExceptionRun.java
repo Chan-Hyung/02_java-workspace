e4ef30bcefb10ba2dd7c9e09ab09b59408e2db07
Chan-Hyung <homework201@naver.com>
2025-07-07 09:12:44 +0900

    Create ExceptionRun.java

---
+++ 10_Exception/src/com/kh/exception/run/ExceptionRun.java
@@ -0,0 +1,15 @@
+package com.kh.exception.run;
+
+public class ExceptionRun {
+
+	public static void main(String[] args) {
+		/*
+		 * - 에러종류
+		 * 1) 시스템 에러 : 컴퓨터의 오작동으로인한 오류 => 스스로 해결 X
+		 * 2) 컴파일 에러 : 소스코드 문법상 오류 => 빨간줄로 애초에 오류로 알려줌(개발자의 실수)
+		 * 3) 런타임 에러 : 코드 상으로는 문제가 없는데 프로그램 실행 중에 발생하는 에러(개발자의 실수 & 사용자의 실수)
+		 */
+
+	}
+
+}
